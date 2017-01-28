755eb8b2d6db34333b708423beba2bf50cdbe100
Jarrid Rector-Brooks <jrectorb@umich.edu>
2017-01-28 00:15:41 -0500

    AnyNull returned opposite of correct bool values

--- Utils.java
+++ Utils.java
@@ -2,10 +2,10 @@ public class Utils {
     public static boolean AnyNull(Object ... objects) {
         for (Object obj : objects) {
             if (obj == null) {
-                return false;
+                return true;
             }
         }
 
-        return true;
+        return false;
     }
 }
