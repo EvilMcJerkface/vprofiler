628e3c2c8c36d8ada7538675da28f9801fc89139
Jarrid Rector-Brooks <jrectorb@umich.edu>
2017-05-29 13:47:38 -0700

    Added untested wrapper script for all of vprof

---
+++ VProfWrapper/AnnotatorDispatcher.py
@@ -0,0 +1,22 @@
+from DispatcherBase import Dispatcher
+
+class Annotator(Dispatcher):
+    def __init__(self):
+        self.disallowedOptions = { 'breakdown': True,
+                                   'restore':   True  }
+
+        self.requiredOptions = { 'source_dir': None,
+                                 'parallelism_functions': None,
+                                 'compilation_db': None,
+                                 'target_fxn': None }
+
+    def Dispatch(self):
+        print('Instrumenting synchronization events')
+        subprocess.call(['EventAnnotator', '-s', self.requiredOptions['source_dir'],
+              '-f', self.requiredOptions['parallelism_functions'], self.requiredOptions['compilation_db']],
+              stderr = subprocess.STDOUT)
+
+        print('Instrumenting user source code')
+        subprocess.call(['TraceInstrumentor', '-s', self.requiredOptions['source_dir'],
+              '-f', self.requiredOptions['target_fxn'], self.requiredOptions['compilation_db']],
+              stderr = subprocess.STDOUT)
