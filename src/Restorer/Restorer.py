27db8f951245d9c50f2e6112543abff665245941
Jiamin Huang <hjmsens@gmail.com>
2017-06-08 18:12:50 -0400

    Bug fix in Restorer.py: restore files in reversed order. Remove restorer and just use Restorer.py from RestorerDispatcher.py.

--- src/Restorer/Restorer.py
+++ src/Restorer/Restorer.py
@@ -1,16 +1,23 @@
-import subprocess
+import os
+import shutil
 import csv
 
 class Restorer:
     def __init__(self, backupDir):
         self.backupDir = backupDir
+        if not self.backupDir.endswith('/'):
+            self.backupDir += '/'
 
-    def Run(self, filenamesListFname):
-        with open(filenamesListFname, 'rb') as fnamesList:
-            filenameReader = csv.reader(fnamesList, delimiter='\t')
+    def Run(self, filenamesListFname, doDelete=False):
+        if not os.path.exists(self.backupDir + filenamesListFname):
+            return
+        with open(self.backupDir + filenamesListFname, 'rb') as fnamesList:
+            filenameReader = reversed(list(csv.reader(fnamesList, delimiter='\t')))
 
             for line in filenameReader:
-                subprocess.call(['cp', line[0], line[1]])
-                subprocess.call(['rm', line[0]])
+                shutil.copyfile(line[0], line[1])
+                if doDelete:
+                    os.remove(line[0])
 
-        subprocess.call(['rm', filenamesListFname])
+        if doDelete:
+            os.remove(self.backupDir + filenamesListFname)
